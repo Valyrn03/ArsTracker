abe54fbdb79d156f996090eddc5d7730d075f918
Valhryn <yoav.maoz0@gmail.com>
2024-12-22 04:03:59 -0500

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.valyrn.arstracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.valyrn.arstracker to javafx.fxml;
+    exports com.valyrn.arstracker;
+}
