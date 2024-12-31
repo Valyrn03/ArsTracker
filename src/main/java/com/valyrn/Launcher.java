7090b3175f3a7590e35b9c0ea9b7c2443c5ba8ab
Valhryn <yoav.maoz0@gmail.com>
2024-12-31 02:14:07 -0500

    Finally working

---
+++ src/main/java/com/valyrn/Launcher.java
@@ -0,0 +1,11 @@
+package com.valyrn;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args){
+        App.launch(App.class, args);
+    }
+}
