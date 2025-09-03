1126f8f164212e7b6efa81820d49e3b597f3c043
valyrn <yoav.maoz0@gmail.com>
2025-09-02 23:17:14 -0400

    Began execute() method for Creator

--- src/main/java/application/commands/CharacterCreator.java
+++ src/main/java/application/commands/CharacterCreator.java
@@ -5,6 +5,7 @@ import application.terminal.CommandFramework;
 import org.beryx.textio.TextIO;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /*
     This command will build off of the editing command. The execute function will run through each requirement, rather than giving options and allowing players to select from those.
@@ -20,6 +21,7 @@ import java.util.ArrayList;
         characteristics*
  */
 public class CharacterCreator extends CharacterEditor{
+    Character character;
 
     public CharacterCreator(CommandFramework framework, ArrayList<Character> arr) {
         super(framework, arr, null);
@@ -27,10 +29,33 @@ public class CharacterCreator extends CharacterEditor{
 
     @Override
     public boolean execute(){
+        String name = super.getString("Name >");
+        int age = super.getInt("Current Age >");
+        int birthSeason = getSeason(age);
+
+        ArrayList<String> characterTypes = new ArrayList<>(List.of(new String[]{"MAGUS", "COMPANION", "GROG"}));
+        String type = characterTypes.get(super.getOptions(characterTypes));
+
+        character = new Character(name, birthSeason, type);
+
+        //Call Editor's Characteristics, Ability, and Feature creation methods
+
+        return addCharacter();
+    }
+
+    /*
+    Add created character to the database
+     */
+    public boolean addCharacter(){
         return false;
     }
 
-    public boolean addCharacter(){
-        return false;
+    /*
+    Method to convert a given age to the season where the character was born
+
+    Will need to implement with different campaigns / birth seasons in mind later
+     */
+    public static int getSeason(int age){
+        return 1220 - age * 4;
     }
 }
